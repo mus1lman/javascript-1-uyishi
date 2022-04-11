2c6b0e4fe916a839e7fc9568a446dcd02937a638
mus1lman <azizovsardor007@gmail.com>
2022-04-11 03:05:35 +0300

    git init.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+alert("Assalomu alaykum.");
+alert("Xush kelibsiz");
+var firstName = prompt("Ismingizni kiriting!");
+var firstlastName = prompt("Familiyangizni kiriting!");
+var age = prompt("Yoshingizni kiriting!");
+var hobby = prompt("Nimalarga qiziqasiz?");
+var job = prompt("Qayerda ishlaysiz  (Yoki o'qiysiz?)");
+alert("Erinmay javob berganingiz uchun rahmat👍");
+alert("Xayr, salomat bo'ling!");
